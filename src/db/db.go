175be7846495dc3302133af56b276bbeadb7e29a
PetrusAriaa <petrusariacr25@gmail.com>
2024-07-12 02:43:03 +0700

    fix: use credential from encoded b64 string

--- src/db/db.go
+++ src/db/db.go
@@ -2,10 +2,12 @@ package db
 
 import (
 	"context"
+	"encoding/base64"
 	"log"
 	"os"
 
 	"cloud.google.com/go/storage"
+	"google.golang.org/api/option"
 )
 
 type DBConnections struct {
@@ -14,8 +16,13 @@ type DBConnections struct {
 }
 
 func NewDBConnections(ctx context.Context) *DBConnections {
+	b64, err := base64.StdEncoding.DecodeString(os.Getenv("GOOGLE_CREDENTIALS_BASE64"))
+	if err != nil {
+		panic(err.Error())
+	}
 
-	s, err := storage.NewClient(ctx)
+	opt := option.WithCredentialsJSON(b64)
+	s, err := storage.NewClient(ctx, opt)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
