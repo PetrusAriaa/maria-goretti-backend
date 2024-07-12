99265def7ab9a1daae4bcc159eb9f191fed0bcc3
PetrusAriaa <petrusariacr25@gmail.com>
2024-07-12 17:54:12 +0700

    fix: delete controllers and use api/ handlers instead

--- lib/main.go
+++ lib/main.go
@@ -13,29 +13,18 @@ import (
 func main() {
 	godotenv.Load(".env.development")
 
-	// ctx := context.Background()
-
-	// client := db.NewDBConnections(ctx)
-	// defer client.CloudStorageClient.Close()
-
-	// srv := service.NewService(client, ctx)
-	// c := controller.NewController(srv)
-
 	mux := http.NewServeMux()
 
 	s := &http.Server{
 		Addr:    "0.0.0.0:8080",
 		Handler: mux,
 	}
-	// mux.HandleFunc("/api/v1/images", c.GetImageList)
-	// mux.HandleFunc("/assets/images/{id}", c.GetImage)
 	mux.HandleFunc("/api/images", h1.Image)
 	mux.HandleFunc("/api/status", h1.Status)
 	mux.HandleFunc("/api/static/img", h2.ImageHandler)
 
 	log.Default().Printf("Server started on http://0.0.0.0:8080")
 	if err := s.ListenAndServe(); err != nil {
-		// client.CloudStorageClient.Close()
 		panic(err.Error())
 	}
 }
